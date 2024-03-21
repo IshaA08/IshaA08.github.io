86603c520183082fd993fbfde08bcf0e40671803
Isha Afzaal <25419186+IshaA08@users.noreply.github.com>
2024-03-21 10:53:24 -0600

    Make empty sections for each nav bar item

---
+++ js/main.js
@@ -0,0 +1,8 @@
+/* Make the header stretch across the whole screen */
+var ready = (callback) => {
+    if (document.readyState != "loading") callback();
+    else document.addEventListener("DOMContentLoaded", callback);
+}
+ready(() => {
+    document.querySelector(".header").style.height = window.innerHeight + "px";
+})
