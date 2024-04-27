8937fdb88b93689798874089a24359c889830507
Isha Afzaal <25419186+IshaA08@users.noreply.github.com>
2024-04-26 22:55:48 -0600

    Implement display mode functionality

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-/* Make the header stretch across the whole screen */
+/***********************
+ Set Header
+************************/
 var ready = (callback) => {
     if (document.readyState != "loading") callback();
     else document.addEventListener("DOMContentLoaded", callback);
@@ -7,7 +9,60 @@ ready(() => {
     document.querySelector(".header").style.height = window.innerHeight + "px";
 })
 
-/* Analog wall clock */
+/***********************
+ Display Mode
+************************/
+function dayMode() {
+    document.documentElement.style.setProperty('--bg', '#e5eef4');
+    document.documentElement.style.setProperty('--font', '#1F2232');
+    document.documentElement.style.setProperty('--primary', '#62c5ff');
+    document.documentElement.style.setProperty('--secondary', '#77c8ff');
+    document.documentElement.style.setProperty('--headline', '#fff');
+    document.documentElement.style.setProperty('--sky-top', '#00b3ff');
+    document.documentElement.style.setProperty('--sky-bottom', '#b1e8ff');
+    document.documentElement.style.setProperty('--stars-bright', '#00b3ff');
+    document.documentElement.style.setProperty('--stars-normal', '#b1e8ff');
+    document.documentElement.style.setProperty('--stars-variation', '#ffffffb3');
+    document.documentElement.style.setProperty('--cards', '#b5bfc4');
+    document.documentElement.style.setProperty('--shadow-primary', '#006ba1');
+    document.documentElement.style.setProperty('--shadow-cards', '#616466');
+}
+
+function eveningMode() {
+    document.documentElement.style.setProperty('--bg', '#F1DAC4');
+    document.documentElement.style.setProperty('--font', '#1F2232');
+    document.documentElement.style.setProperty('--primary', '#FFB7C5');
+    document.documentElement.style.setProperty('--secondary', '#f8bec8');
+    document.documentElement.style.setProperty('--headline', '#fff');
+    document.documentElement.style.setProperty('--sky-top', '#ff879f');
+    document.documentElement.style.setProperty('--sky-bottom', '#ffe3e9');
+    document.documentElement.style.setProperty('--stars-bright', '#ffafc7');
+    document.documentElement.style.setProperty('--stars-normal', '#ffbbd4e1');
+    document.documentElement.style.setProperty('--stars-variation', '#ffffffb3');
+    document.documentElement.style.setProperty('--cards', '#f0e2d2');
+    document.documentElement.style.setProperty('--shadow-primary', '#fa96ae');
+    document.documentElement.style.setProperty('--shadow-cards', '#f0bf84');
+}
+
+function nightMode() {
+    document.documentElement.style.setProperty('--bg', '#6873a5');
+    document.documentElement.style.setProperty('--font', '#f8ebf6');
+    document.documentElement.style.setProperty('--primary', '#224597');
+    document.documentElement.style.setProperty('--secondary', '#21335b');
+    document.documentElement.style.setProperty('--headline', '#240d4f');
+    document.documentElement.style.setProperty('--sky-top', '#040f19');
+    document.documentElement.style.setProperty('--sky-bottom', '#152247');
+    document.documentElement.style.setProperty('--stars-bright', '#fff692');
+    document.documentElement.style.setProperty('--stars-normal', '#f1eba6');
+    document.documentElement.style.setProperty('--stars-variation', '#d8fcff');
+    document.documentElement.style.setProperty('--cards', '#766c89');
+    document.documentElement.style.setProperty('--shadow-primary', '#080311');
+    document.documentElement.style.setProperty('--shadow-cards', '#151517');
+}
+
+/***********************
+ Analog Wall Clock
+************************/
 const secondHand = document.querySelector('.second-hand');
 const minsHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
@@ -31,5 +86,3 @@ function setDate() {
 setInterval(setDate, 1000);
 
 setDate();
-
-/* Contact Form */
